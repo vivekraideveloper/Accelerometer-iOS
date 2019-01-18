c3c3db36362697e75095100352813538bcac6d15
Vivek Rai <vivekraiiosdev@gmail.com>
2019-01-19 03:16:20 +0530

    Accelorometer App first commit

--- Accelorometer/ViewController.swift
+++ Accelorometer/ViewController.swift
@@ -7,13 +7,43 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+    var motionManager: CMMotionManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLabels)
+        
     }
+    
+    func updateLabels(data: CMAccelerometerData?, error: Error?){
+        guard let accelorometerData = data else { return }
+        
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 1
+        formatter.maximumFractionDigits = 2
+        
+        let x = formatter.string(for: accelorometerData.acceleration.x)
+        let y = formatter.string(for: accelorometerData.acceleration.y)
+        let z = formatter.string(for: accelorometerData.acceleration.z)
+
+        
+        xLabel.text = "X : " + x!
+        yLabel.text = "Y : " + y!
+        zLabel.text = "Z : " + z!
+        
+    }
+    
+    
 
 
 }
